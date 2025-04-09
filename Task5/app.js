175e469a4a5363be889b408cde51487b905d44de
AlexanderSharban <alex.sharb4@gmail.com>
2025-04-09 12:35:12 +0300

    Tasks 5

--- Task5/app.js
+++ Task5/app.js
@@ -1,1 +1,7 @@
-аааа 
+function objectToArray(obj) {
+  return Object.entries(obj);
+}
+
+console.log(objectToArray({ name: 'Alice', age: 25})); 
+
+console.log(objectToArray({ city: 'New York', country: 'USA'}));
