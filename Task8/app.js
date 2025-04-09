bcfe0eea5785ea13f04bc00673c9893a69eb75dd
AlexanderSharban <alex.sharb4@gmail.com>
2025-04-09 13:05:18 +0300

    Tasks 8

--- Task8/app.js
+++ Task8/app.js
@@ -1,1 +1,18 @@
-аааа 
+function startLoading() {
+    let progress = 0;
+    const progressBar = document.getElementById('progressBar');
+    const statusText = document.getElementById('statusText');
+    
+    const interval = setInterval(() => {
+      progress += 1; 
+      
+      progressBar.style.width = progress + '%';
+      statusText.textContent = `Прогресс: ${progress}%`;
+      
+      if (progress >= 100) {
+        clearInterval(interval);
+      }
+    }, 100); 
+}
+  
+  document.getElementById('startButton').addEventListener('click', startLoading);s
