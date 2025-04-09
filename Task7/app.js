af79157833c01fb562697e25d5163fb5c642adb6
AlexanderSharban <alex.sharb4@gmail.com>
2025-04-09 13:08:58 +0300

    Tasks 7

--- Task7/app.js
+++ Task7/app.js
@@ -1,1 +1,9 @@
-аааа 
+function addListItem() {
+    const ul = document.getElementById('myList'); 
+    const li = document.createElement('li'); 
+    li.textContent = 'New Item'; 
+    ul.appendChild(li); 
+}
+
+  document.getElementById('addButton').addEventListener('click', addListItem);
+  
