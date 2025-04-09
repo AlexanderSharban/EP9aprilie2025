604b5cf03ffb787fb5b83c61114d182c69133c20
AlexanderSharban <alex.sharb4@gmail.com>
2025-04-09 12:27:20 +0300

    Tasks 3-4

---
+++ Task3/app.js
@@ -0,0 +1,12 @@
+function reverseArray(arr) {
+    let reversedArr = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedArr.push(arr[i]);
+    }
+
+    return reversedArr;
+}
+
+console.log(reverseArray([1, 2, 3, 4]));
+console.log(reverseArray(['a', 'b', 'c'])); 
--- Task4/app.js
+++ Task4/app.js
@@ -1,1 +1,14 @@
-аааа 
+function countOccurrences(arr, value) {
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+ console.log(countOccurrences([1, 2, 3, 1, 2, 1], 1)); 
+console.log(countOccurrences(['a', 'b', 'a', 'c'], 'a')); 
